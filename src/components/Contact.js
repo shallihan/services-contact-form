5e5ea652503bce99d13fd7bf64badcfbf85166fa
shallihan <smhallihan@gmail.com>
2021-06-08 09:44:52 +0100

    Setting up basic structure of contact component.

---
+++ src/components/Contact.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>Schedule an Appointment</h2>
+      <form>
+        <p>Hey, my name is </p>
+        <input type="text" />
+        <p> and I'd like to make an appointment with</p>
+        <select>
+          <option value="David">David</option>
+          <option value="Johanna">Johanna</option>
+          <option value="Stephen">Stephen</option>
+        </select>
+        <p>You can contact me at </p>
+        <input type="email" />
+        <div className="terms-conditions">
+          <input type="radio" value="accept" />
+          <p>I hereby accept all terms and conditions</p>
+        </div>
+        <button type="submit">Send Enquiry</button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+const Home = () => {
+    return (
+        <motion.div></motion.div>
+    );
+}
+
+export default Home;
